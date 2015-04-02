71147cf7a1455d6fb3bfc1952e363ff1e4be48f8
Corentin Regal <corentin.regal@gmail.com>
2015-04-02 11:08:37 +0200

    Imp and Dwarf are working

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use Instruction::{ DAT, MOV, ADD, SUB, MUL, DIV, MOD, JMP, JMZ, JMN, DJN, CMP, S
 use Modifier::{ A, B, AB, BA, F, X, I };
 
 // Address modes
-#[derive(Clone)]
+#[derive(PartialEq, Clone)]
 enum Address {
     Immediate,       // #
     Direct,          // $
@@ -19,7 +19,7 @@ enum Address {
 }
 
 // Instructions
-#[derive(Clone)]
+#[derive(PartialEq, Clone)]
 enum Instruction {
     DAT,
     MOV,
@@ -38,7 +38,7 @@ enum Instruction {
 }
 
 // Modifiers
-#[derive(Clone)]
+#[derive(PartialEq, Clone)]
 enum Modifier {
     A,
     B,
@@ -52,11 +52,11 @@ enum Modifier {
 #[derive(Clone)]
 struct Param {
     addr: Address,
-    val:  usize,
+    val:  i32,
 }
 
 impl Param {
-    fn new(addr: Address, val: usize) -> Param {
+    fn new(addr: Address, val: i32) -> Param {
         Param { addr: addr, val: val }
     }
 }
@@ -70,7 +70,38 @@ struct Cell {
 }
 
 impl Cell {
-    fn new(ins: Instruction, modifier: Modifier, a: Param, b: Param) -> Cell {
+    fn new(ins: Instruction, opt_modifier: Option<Modifier>, a: Param, b: Param) -> Cell {
+        let modifier = match opt_modifier {
+            Some(modifier) => modifier,
+            // ICWS'88 to ICWS'94 conversion
+            None           => {
+                match ins {
+                    DAT                         => F,
+                    MOV | CMP                   => {
+                        match (&a.addr, &b.addr) {
+                            (&Immediate, _) => AB,
+                            (_, &Immediate) => B,
+                            _               => I,
+                        }
+                    },
+                    ADD | SUB | MUL | DIV | MOD => {
+                        match (&a.addr, &b.addr) {
+                            (&Immediate, _) => AB,
+                            (_, &Immediate) => B,
+                            _               => F,
+                        }
+                    },
+                    SLT                         => {
+                        if a.addr == Immediate {
+                            AB
+                        } else {
+                            B
+                        }
+                    },
+                    JMP | JMZ | JMN | DJN | SPL => B,
+                }
+            }
+        };
         Cell { ins: ins, modifier: modifier, a: a, b: b }
     }
 }
@@ -82,12 +113,14 @@ struct Mars {
 
 impl Mars {
     fn new(size: usize) -> Mars {
-        let default = Cell::new(DAT, F, Param::new(Immediate, 0), Param::new(Immediate, 0));
+        let default = Cell::new(DAT, None, Param::new(Immediate, 0), Param::new(Immediate, 0));
         Mars { core: vec![default; size], iptr: 0 }
     }
 
     fn load(&mut self, warrior: Vec<Cell>) {
-        self.core[0..warrior.len()] = warrior[..];
+        for (idx, cell) in warrior.iter().enumerate() {
+            self.core[idx] = cell.clone();
+        }
     }
 
     fn run(&mut self) {
@@ -99,7 +132,13 @@ impl Mars {
                 if cptr == self.iptr {
                     print!("\x1B[0;31m");
                 }
-                print!("_");
+                match self.core[cptr].ins {
+                    DAT => print!("{} ", self.core[cptr].b.val),
+                    MOV => print!("M "),
+                    ADD => print!("A "),
+                    JMP => print!("J "),
+                    _   => print!("U "),
+                };
                 if cptr == self.iptr {
                     print!("\x1B[0;0m");
                 }
@@ -111,49 +150,77 @@ impl Mars {
         }
     }
 
+    fn param<F>(&self, iptr: usize, get_param: F) -> usize
+        where F: Fn(&Cell) -> &Param {
+
+        let ref cell = self.core[iptr];
+        let param = get_param(cell);
+        let ptr = match param.addr {
+            Immediate       => 0,
+            Direct          => param.val,
+            Indirect        => {
+                let ptrcell = self.add_iptr(iptr, param.val);
+                param.val + get_param(&self.core[ptrcell]).val
+            },
+            PreIncIndirect  => panic!("PreIncIndirect not implemented"),                            // TODO
+            PostIncIndirect => panic!("PostIncIndirect not implemented"),                           // TODO
+        };
+        self.add_iptr(iptr, ptr)
+    }
+
     fn execute(&mut self, iptr: usize) -> usize {
         let cell = self.core[iptr].clone();
 
         // Parameters
-        let aptr = match cell.a.addr {
-            Immediate => 0,
-            Direct    => cell.a.val,
-            _         => panic!("Address not implemented"),
-        };
-        let bptr = match cell.b.addr {
-            Immediate => 0,
-            Direct => cell.b.val,
-            _      => panic!("Address not implemented"),
-        };
+        let aptr = self.param(iptr, |c| &c.a);
+        let bptr = self.param(iptr, |c| &c.b);
 
         // Instruction
         match cell.ins {
-            DAT => { },
+            DAT => self.add_iptr(iptr, 1),
             MOV => {
                 match cell.modifier {
                     I => {
-                        let src = self.add_iptr(iptr, aptr);
-                        let dst = self.add_iptr(iptr, bptr);
-                        self.core[dst] = self.core[src].clone()
+                        self.core[bptr] = self.core[aptr].clone();
+                        self.add_iptr(iptr, 1)
                     },
                     _ => panic!("Modifier not implemented"),
                 }
+            },
+            ADD => {
+                match cell.modifier {
+                    AB => {
+                        let sum = self.core[aptr].a.val + self.core[bptr].b.val;
+                        self.core[bptr].b.val = sum;
+                        self.add_iptr(iptr, 1)
+                    },
+                    _ => panic!("Modifier not implemented"),
+                }
+            },
+            JMP => {
+                aptr
             }
             _   => panic!("Instruction not implemented"),
         }
-
-        self.add_iptr(iptr, 1)
     }
 
-    fn add_iptr(&self, iptr: usize, add: usize) -> usize {
-        (iptr + add) % self.core.len()
+    fn add_iptr(&self, iptr: usize, add: i32) -> usize {
+        (iptr as i32 + add + self.core.len() as i32) as usize % self.core.len()
     }
 }
 
 fn main() {
-    let mut mars = Mars::new(16);
+    let mut mars = Mars::new(32);
+    // // Imp
+    // mars.load(vec![
+    //     Cell::new(MOV, None, Param::new(Direct, 0), Param::new(Direct, 1)),
+    // ]);
+    // Dwarf
     mars.load(vec![
-        Cell::new(MOV, I, Param::new(Direct, 0), Param::new(Direct, 1))
+        Cell::new(ADD, None, Param::new(Immediate, 4), Param::new(Direct, 3)),
+        Cell::new(MOV, None, Param::new(Direct, 2), Param::new(Indirect, 2)),
+        Cell::new(JMP, None, Param::new(Direct, -2), Param::new(Immediate, 0)),
+        Cell::new(DAT, None, Param::new(Immediate, 0), Param::new(Immediate, 0))
     ]);
     mars.run();
     println!("Hello, world!");
